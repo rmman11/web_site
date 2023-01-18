76760bd74ccc0641dbe591307c7d184b889c7ef0
rmman11 <38256101+rmman11@users.noreply.github.com>
2023-01-18 17:47:36 +0200

    Update head.blade.php

--- resources/views/admin/partial/head.blade.php
+++ resources/views/admin/partial/head.blade.php
@@ -9,7 +9,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <title>@yield('title')</title>
         <link href="/css/styles.css" rel="stylesheet" />
-        <link href="/css/main.css" rel="stylesheet" />
+        <!--<link href="/css/main.css" rel="stylesheet" />-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
       
        <!-- <link href="https://cdn.jsdelivr.net/npm/daisyui@2.31.0/dist/full.css" rel="stylesheet" type="text/css" />
